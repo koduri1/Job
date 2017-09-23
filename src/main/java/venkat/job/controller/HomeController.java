c0e22686bc141c4bfe00236e7b0392feca717b9e
syam-nd <syam.krishna@netradyne.com>
2017-09-23 18:06:33 +0530

    Rest API fixed

--- src/main/java/venkat/job/controller/HomeController.java
+++ src/main/java/venkat/job/controller/HomeController.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
 
-	@RequestMapping(value="/")
-	public ModelAndView test(HttpServletResponse response) throws IOException{
+	@RequestMapping(value = "/")
+	public ModelAndView test(HttpServletResponse response) throws IOException {
 		return new ModelAndView("home");
 	}
-	@RequestMapping(value="/test", Method=HttpMethod.GET)
-	public String test123(HttpServletResponse response) throws IOException{
-	return "test123";
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test123(HttpServletResponse response) throws IOException {
+		return "test123";
 	}
 }
